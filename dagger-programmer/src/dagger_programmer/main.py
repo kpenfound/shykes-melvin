fd0c2f3b27f5a3fec22ef744c56e5b03c62efc95
kpenfound <kyle@dagger.io>
2025-02-27 17:22:24 -0500

    change translator to take in mod as a dir instead of repo url and move prompt to a file
    Signed-off-by: kpenfound <kyle@dagger.io>

--- dagger-programmer/src/dagger_programmer/main.py
+++ dagger-programmer/src/dagger_programmer/main.py
@@ -7,17 +7,14 @@ class DaggerProgrammer:
     @function
     async def translate(
         self,
-        repo: Annotated[str, Doc("The git repo of the module to translate")],
+        mod: Annotated[Directory, Doc("The dagger module to translate")],
         language: Annotated[str, Doc("The language to translate the module to")],
         subpath: Annotated[str, Doc("The path to the module in the git repository")] = ""
     ) -> Directory:
         """Returns a dagger module in the specified language translated from the provided module"""
-        source_dir = dag.git(repo).head().tree()
-        if subpath != "":
-            source_dir = source_dir.directory(subpath)
 
-        source_mod_sdk = await source_dir.as_module().sdk().source()
-        source_mod_name = await source_dir.as_module().name()
+        source_mod_sdk = await mod.as_module().sdk().source()
+        source_mod_name = await mod.as_module().name()
 
         # Create a mod/workspace for the translated sdk
         ws = dag.module_workspace(language, source_mod_name)
@@ -28,34 +25,26 @@ class DaggerProgrammer:
             "php": "src/MyModule.php",
             "java": "src/main/java/io/dagger/modules/mymodule/MyModule.java"
         }
-        source_mod_file = await source_dir.file(main_file_paths[source_mod_sdk]).contents()
+        source_mod_file = await mod.file(main_file_paths[source_mod_sdk]).contents()
 
         # translate the source mod to the target sdk
         work = (
             dag
             .llm()
             .with_module_workspace(ws)
-            .with_prompt(f"""
-You are an expert translating Dagger code between {source_mod_sdk} and {language} SDKs.
-Use the 'getReference' tool to learn how to use each Dagger SDK.
-You have access to a workspace with the ability to read, write, and test code.
-Before writing code, always use `getReference` for both SDKs you are working with.
-Use the reference snippets to help you translate the assignment below.
-Always write the translated code to the workspace you've been provided.
-Always run test after writing the code to ensure it passes.
-If test fails, read the error message and fix the code until it passes.
-Do not stop until the you have translated the provided code and test passes.
-
-Your assignment is to translate the following code from {source_mod_sdk} to {language} and write the code to your workspace:
-<assignment>
-{source_mod_file}
-</assignment>
-
-ALWAYS WRITE YOUR GENERATED CODE TO THE WORKSPACE PROVIDED. TEST MUST PASS.
-            """)
+            .with_prompt_var("language", language)
+            .with_prompt_var("source_mod_sdk", source_mod_sdk)
+            .with_prompt_var("source_mod_file", source_mod_file)
+            .with_prompt_file(dag.current_module().source().file("prompt_translator.txt"))
             .module_workspace()
         )
         # Check again that test passes because LLMs lie
         await work.test()
         # return work output
         return work.workspace()
+
+    @function
+    async def write_examples(
+        self,
+    ) -> Directory:
+        return await dag.directory()
